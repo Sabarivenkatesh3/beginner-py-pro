fdaa171820443b8614b0b126c52da9a9e4dacf05
sabarivarun3 <sabarivarun3@gmail.com>
2025-09-25 01:27:49 +0530

    fixed errors

--- src/pages/Lessons.tsx
+++ src/pages/Lessons.tsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!  // 👈 fix here
 );
 
 interface Lesson {
